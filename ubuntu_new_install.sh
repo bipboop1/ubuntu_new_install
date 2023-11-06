5989d2657479e3c8b547b5bab4c0c48aae8910ff
bipboop1 <sel-bouh@student.42.fr>
2023-11-06 21:45:10 +0100

    tbs

--- ubuntu_new_install.sh
+++ ubuntu_new_install.sh
@@ -108,10 +108,10 @@ if [ "$choice" = "1" ]; then
 elif [ "$choice" = "2" ]; then
 	skip_to_section
 elif [ "$choice" = "3" ]; then
-
+	full_list_menu
 elif [ "$choice" = "0" ]; then
 	echo -e "${BOLD_YELLOW}Exiting program.${NC}"
-#	exit 0
+	exit 0
 else
 	echo -e "${BOLD_RED}Invalid answer. Launching full installer.${NC}"
 fi
