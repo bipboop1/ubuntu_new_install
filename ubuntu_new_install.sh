7b2e23efa42fc26fcd6bc591f07d50f61f56b3ff
bipboop1 <sel-bouh@student.42.fr>
2023-11-06 17:34:43 +0100

    fixed signal install

--- ubuntu_new_install.sh
+++ ubuntu_new_install.sh
@@ -215,7 +215,7 @@ if confirm_install "Signal"; then
  	 sudo tee /etc/apt/sources.list.d/signal-xenial.list
 
 # 3. Update your package database and install Signal:
-	sudo apt install signal-desktop
+	sudo snap install signal-desktop
 fi
 
 # Install games
